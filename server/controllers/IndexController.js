43fa682805362ed23082ed9cadb7582d82ca33b2
GuilhermeLima <guilherme.lima@sigep.com.br>
2022-07-08 09:48:18 -0300

    feat:Configurando Rotas/controller

--- server/controllers/IndexController.js
+++ server/controllers/IndexController.js
@@ -3,7 +3,25 @@ const controller = {
         res.render('index', { title: 'Skiline Pra_Valer' });
     },
     posts: function (req, res, next) {
-        res.render('posts', { title: 'Skiline Pra_Valer' });
+        res.render('posts', { title: 'postagens recentes' });
+    },
+    alerts: function (req, res, next) {
+        res.render('alerts', { title: 'Alertas' });
+    },
+    explorer: function (req, res, next) {
+        res.render('explorer', { title: 'explorar' });
+    },
+    login: function (req, res, next) {
+        res.render('login', { title: 'login' });
+    },
+    more: function (req, res, next) {
+        res.render('more', { title: 'more' });
+    },
+    profile: function (req, res, next) {
+        res.render('profile', { title: 'Perfil' });
+    },
+    savedItems: function (req, res, next) {
+        res.render('savedItems', { title: 'Itens Salvos' });
     }
 
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,4 +5,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', controller.index);
 router.get('/posts', controller.posts);
+router.get('/alerts', controller.alerts);
+router.get('/explorer', controller.explorer);
+router.get('/login', controller.login);
+router.get('/more', controller.more);
+router.get('/profile', controller.profile);
+router.get('/savedItems', controller.savedItems);
 module.exports = router;
